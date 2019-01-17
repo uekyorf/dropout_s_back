6ed75dd73ed1501cfb17ee5dca6e5f69ee0f0e48
EIMIKI <kou.103.kan@gmail.com>
2019-01-17 23:08:40 +0900

    ルーティングを初期化し、サーバを起動するように

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"dropout_s_back/config"
 	"dropout_s_back/db"
 )
 
@@ -8,4 +9,7 @@ func main() {
 	db.Init()            // DB接続初期化
 	conn := db.GetConn() // DB接続取得
 	defer conn.Close()
+
+	r := config.Init(conn) // routes初期化
+	r.Run(":3000")         // サーバ起動
 }
