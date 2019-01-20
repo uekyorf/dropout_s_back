b805e30b2b44b3726dac394683432f06ab074334
uekyorf <uekyorf@gmail.com>
2019-01-21 00:04:55 +0900

    messagesテーブルへINSERTする処理を追加

--- controller/messageController.go
+++ controller/messageController.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"dropout_s_back/db"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,6 +25,33 @@ type PostRequestJson struct {
 
 // PostMessage 要求に基づいてメッセージをデータベースに登録する
 func (ctrler Controller) PostMessage(c *gin.Context) {
-	//db := ctrler.conn //DB接続
+	//DB接続
+	conn := ctrler.conn
+	defer conn.Close()
+
+	// リクエストをバインド
+	req := PostRequestJson{}
+	c.BindJSON(&req)
+	// リクエストの内容を基にSELECT
+	device := db.Device{}
+	conn.First(&device, "name=?", req.Device_name)
+	user := db.User{}
+	conn.First(&user, device.UserID)
+	ble := db.Ble{}
+	conn.First(&ble, "name=?", req.Ble_uuid)
+	// messageを作成し、INSERT
+	message := db.Message{}
+	message.UserID = user.ID
+	message.Title = req.Title
+	message.Body = req.Body
+	message.BleID = ble.ID
+	if req.Due == "" {
+		t := time.Now()
+		req.Due = t.Format("2006-01-02-15-04")
+	}
+	message.Due, _ = time.Parse("2006-01-02-15-04-05 MST", req.Due+"-00 JST")
+	message.Due = message.Due.AddDate(0, 1, 0)
+	conn.Create(&message)
+
 
 }
