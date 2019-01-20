8e99c3679ce4c452b95b5c83bd0e5eed98d5fdc1
uekyorf <uekyorf@gmail.com>
2019-01-20 23:52:54 +0900

    リクエストの構造体を定義

--- controller/messageController.go
+++ controller/messageController.go
@@ -10,6 +10,16 @@ func (ctrler Controller) GetMessage(c *gin.Context) {
 
 }
 
+// PostRequestの構造体
+type PostRequestJson struct {
+	Device_name string   `json:"device_name"`
+	Title       string   `json:"title"`
+	Body        string   `json:"body"`
+	Due         string   `json:"due"`
+	Ble_uuid    string   `json:"ble_uuid"`
+	To_user     []string `json:"to_user"`
+}
+
 // PostMessage 要求に基づいてメッセージをデータベースに登録する
 func (ctrler Controller) PostMessage(c *gin.Context) {
 	//db := ctrler.conn //DB接続
