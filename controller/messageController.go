048eb308c45e993e6e827ec461a3aee29a04a66c
uekyorf <uekyorf@gmail.com>
2019-01-23 19:23:22 +0900

    リクエストの構造体に全員へ送信するフラグを追加

--- controller/messageController.go
+++ controller/messageController.go
@@ -16,12 +16,13 @@ func (ctrler Controller) GetMessage(c *gin.Context) {
 
 // PostRequestの構造体
 type RequestMessagePost struct {
-	Device_name string   `json:"device_name"`
-	Title       string   `json:"title"`
-	Body        string   `json:"body"`
-	Due         string   `json:"due"`
-	Ble_uuid    string   `json:"ble_uuid"`
-	To_user     []string `json:"to_user"`
+	Device_name  string   `json:"device_name"`
+	Title        string   `json:"title"`
+	Body         string   `json:"body"`
+	Due          string   `json:"due"`
+	Ble_uuid     string   `json:"ble_uuid"`
+	To_user      []string `json:"to_user"`
+	To_all_users bool     `json:"to_all_users" binding:"exists"`
 }
 
 // PostMessage 要求に基づいてメッセージをデータベースに登録する
